3dfbbf33b3b0306eb02a40573ebd49b4fd70637b
Valmir Júnior <valmirgmj@gmail.com>
2024-11-07 01:18:31 -0300

    Replace storage strategy

--- src/modules/planning/PlanningService.ts
+++ src/modules/planning/PlanningService.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import GetAllResponseModel from 'src/modules/planning/model/GetAllResponseModel';
 import BaseService from '../../BaseService';
-import calendar from './calendar.json';
 import GetByYearAndMonthResponseModel from './model/GetByYearAndMonthResponseModel';
 import GetByDateResponseModel from './model/GetByDateResponseModel';
+import BlobManagerFactory from 'src/handlers/blob/BlobManagerFactory';
+import BlobManagerInterface from 'src/handlers/blob/BlobManagerInterface';
 
 @Injectable()
 export default class PlanningService extends BaseService {
+    private readonly blobManager: BlobManagerInterface;
+
     constructor() {
         super();
+        this.blobManager = BlobManagerFactory.createManager()
     }
 
     async getAll(): Promise<GetAllResponseModel> {
+        const calendarBuffer = await this.blobManager.read("calendar.json");
+        const calendarStr = calendarBuffer.toString('binary');
+        const calendar = JSON.parse(calendarStr);
+
         const response: GetAllResponseModel = { items: calendar };
 
         return Promise.resolve(response);
@@ -21,6 +29,10 @@ export default class PlanningService extends BaseService {
         year: number,
         month: number,
     ): Promise<GetByYearAndMonthResponseModel> {
+        const calendarBuffer = await this.blobManager.read("calendar.json");
+        const calendarStr = calendarBuffer.toString('binary');
+        const calendar = JSON.parse(calendarStr);
+
         const selectedYear = calendar[year];
         const selectedMonth: Record<string, string[]> = selectedYear[month];
 
@@ -34,6 +46,10 @@ export default class PlanningService extends BaseService {
         month: number,
         day: number,
     ): Promise<GetByDateResponseModel> {
+        const calendarBuffer = await this.blobManager.read("calendar.json");
+        const calendarStr = calendarBuffer.toString('binary');
+        const calendar = JSON.parse(calendarStr);
+
         const selectedYear = calendar?.[year];
         const selectedMonth = selectedYear?.[month];
         const selectedDay: string[] = selectedMonth?.[day];
