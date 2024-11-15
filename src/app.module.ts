2f0563886f6576fb032c02864fe9814f2e900291
Valmir G. M. Júnior <valmirjunior@velocityglobal.com>
2024-11-15 02:04:15 -0300

    Remove unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 import { ArtifactsModule } from './artifacts.module';
 import { AssistantModule } from './assistant.module';
 import { PlanningModule } from './planning.module';
-import { TasksModule } from './tasks.module';
 
 @Module({
-    imports: [AssistantModule, TasksModule, PlanningModule, ArtifactsModule],
+    imports: [AssistantModule, PlanningModule, ArtifactsModule],
 })
-export class AppModule {}
+export class AppModule { }
--- src/modules/tasks/dto/GetTasksResponseDto.ts
+++
@@ -1,9 +0,0 @@
-import { TaskCategory } from 'src/types/tasks';
-
-export default class GetTasksResponseDto {
-    tasksCategories: TaskCategory[];
-
-    constructor(tasksCategories: TaskCategory[]) {
-        this.tasksCategories = tasksCategories;
-    }
-}
