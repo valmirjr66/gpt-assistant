9c173271bfdb26fba4679281351df037eb28488f
Bruno Soares <burnos@onisys.com.br>
2024-11-18 01:35:41 -0300

    Add TelegramController on AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AssistantModule } from './assistant.module';
+import { TelegramController } from './modules/telegram/TelegramController';
 
 @Module({
     imports: [AssistantModule],
+    controllers: [TelegramController]
 })
 export class AppModule { }
