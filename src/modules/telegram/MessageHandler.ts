9b5f02d765f662e0ba0253b72adb6c44e7d115a0
Valmir G. M. Júnior <valmirjunior@velocityglobal.com>
2024-11-18 21:53:13 -0300

    Just formatting stuff

--- src/modules/telegram/MessageHandler.ts
+++ src/modules/telegram/MessageHandler.ts
@@ -2,48 +2,41 @@ import axiosInstance from './TelegramService';
 
 async function getChatAnswer(messageObj, messageText) {
     return await axiosInstance.sendChatMessage(
-        'api/assistant/conversation/message', {
-            role: "user",
+        'api/assistant/conversation/message',
+        {
+            role: 'user',
             content: messageText,
-            conversationId: messageObj.chat.id.toString()
-        }
+            conversationId: messageObj.chat.id.toString(),
+        },
     );
 }
 
 function sendMessage(messageObj, messageText) {
-    return axiosInstance.get("sendMessage", {
+    return axiosInstance.get('sendMessage', {
         chat_id: messageObj.chat.id,
         text: messageText,
-        }
-    );
+    });
 }
 
 async function handleMessage(messageObj) {
-    const messageText = messageObj.text || "";
+    const messageText = messageObj.text || '';
 
-    if (messageText.charAt(0) === "/") {
-        const command = messageText.substr(1)
+    if (messageText.charAt(0) === '/') {
+        const command = messageText.substr(1);
         if (command == 'start') {
             return sendMessage(
                 messageObj,
-                "Oi, eu sou o chat bot da Witness. Pergunte qualquer coisa e irei te ajudar a encontrar uma resposta."
+                'Oi, eu sou o chat bot da Witness. Pergunte qualquer coisa e irei te ajudar a encontrar uma resposta.',
             );
         } else {
             return sendMessage(
                 messageObj,
-                "Desculpe, eu não conheço esse comando, caso esteja enviando uma pergunta, favor retirar a barra '/' do início da mensagem"
+                "Desculpe, eu não conheço esse comando, caso esteja enviando uma pergunta, favor retirar a barra '/' do início da mensagem",
             );
         }
     } else {
-        const chatMessage = await getChatAnswer(messageObj, messageText)
-        return sendMessage(
-            messageObj,
-            chatMessage.data.content
-        );
-        // return sendMessage(
-        //     messageObj,
-        //     messageText
-        // );
+        const chatMessage = await getChatAnswer(messageObj, messageText);
+        return sendMessage(messageObj, chatMessage.data.content);
     }
 }
 
--- src/modules/telegram/TelegramController.ts
+++ src/modules/telegram/TelegramController.ts
@@ -1,39 +1,35 @@
 import { Controller, Get, Post, Body, Req } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import handleMessage from "./MessageHandler";
+import handleMessage from './MessageHandler';
 
-@ApiTags('Telegram bot')
+@ApiTags('Telegram Bot')
 @Controller('telegram')
 export class TelegramController {
-  @Post()
-  async handlePost(@Body() body: any): Promise<any> {
-    // console.log("POST body:", body);
+    @Post()
+    async handlePost(@Body() body: any): Promise<any> {
+        const cleanedBody = { ...body };
 
-    const cleanedBody = { ...body };
-
-    return await this.handler(cleanedBody, 'post');
-  }
-
-  @Get()
-  async handleGet(@Req() req: any): Promise<any> {
-    // console.log("GET request:", req);
-
-    const simplifiedReq = {
-      method: req.method,
-      headers: req.headers,
-      url: req.url,
-    };
-
-    return await this.handler(simplifiedReq, 'get');
-  }
-
-  private async handler(req: any, method: string): Promise<any> {
-    const body  = req;
-
-    if (body) {
-        const messageObj = body.message;
-        await handleMessage(messageObj);
+        return await this.handler(cleanedBody);
     }
-    return;
-  }
+
+    @Get()
+    async handleGet(@Req() req: any): Promise<any> {
+        const simplifiedReq = {
+            method: req.method,
+            headers: req.headers,
+            url: req.url,
+        };
+
+        return await this.handler(simplifiedReq);
+    }
+
+    private async handler(req: any): Promise<any> {
+        const body = req;
+
+        if (body) {
+            const messageObj = body.message;
+            await handleMessage(messageObj);
+        }
+        return;
+    }
 }
