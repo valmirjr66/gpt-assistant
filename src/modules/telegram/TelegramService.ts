09f326872cb3f1fe820205229266cae144fa0c54
Bruno Soares <burnos@onisys.com.br>
2024-11-20 02:39:15 -0300

    Returns an error when message don't have a text

--- src/modules/telegram/TelegramService.ts
+++ src/modules/telegram/TelegramService.ts
@@ -7,24 +7,21 @@ const APP_API_URL = process.env.APP_API_URL;
 function getAxiosInstance() {
     return {
         async get(method, params) {
-            console.log('get')
             return await axios.get(
                 `/${method}`, {
-                    baseURL: BASE_URL,
-                    params,
+                baseURL: BASE_URL,
+                params,
             });
         },
         async post(method, data) {
-            console.log('post')
             return await axios.post(
                 `/${method}`, {
-                    baseURL: BASE_URL,
-                    URL: `/${method}`,
-                    data
+                baseURL: BASE_URL,
+                URL: `/${method}`,
+                data
             });
         },
         async sendChatMessage(method, params) {
-            console.log('sendChatMessage')
             return await axios.post(
                 `${APP_API_URL}/${method}`,
                 params
