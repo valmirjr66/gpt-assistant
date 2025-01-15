7399dc19c9951b57e9d212c3b31ed4718d6ed1b2
Valmir Júnior <valmirgmj@gmail.com>
2025-01-15 20:01:46 -0300

    Remove injectable decorator from BlobManagerFactory

--- src/handlers/blob/BlobManagerFactory.ts
+++ src/handlers/blob/BlobManagerFactory.ts
@@ -1,8 +1,6 @@
-import { Injectable } from '@nestjs/common';
 import BlobManagerInterface from './BlobManagerInterface';
 import CloudBlobManager from './CloudBlobManager';
 
-@Injectable()
 export default class BlobManagerFactory {
     static createManager(): BlobManagerInterface {
         return new CloudBlobManager();
