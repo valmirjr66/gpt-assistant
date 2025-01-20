e97825cb9f4bc5ae780750e5ba807706791b3b72
Valmir Júnior <valmirgmj@gmail.com>
2025-01-19 21:47:09 -0300

    Remove unused module

--- src/artifacts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import ArtifactsController from './modules/artifacts/ArtifactsController';
-import ArtifactsService from './modules/artifacts/ArtifactsService';
-
-@Module({
-    controllers: [ArtifactsController],
-    providers: [ArtifactsService],
-})
-export class ArtifactsModule {}
